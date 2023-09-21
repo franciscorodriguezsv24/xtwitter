53eedbf6b080ba230a549fd84a31fea56d691271
franciscoWork <frodriguez@ioogo.com>
2023-09-21 11:59:44 -0600

    work and understanding the validation

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,3 +1,7 @@
 class Follower < ApplicationRecord
     has_and_belongs_to :authors
+
+    # Add uniqueness validation for follower_id and follwee_id, a user can only follows another user once
+
+    # In additional to the validations requested above, you should add association validations to all your model
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :author
+  
+
+  # Add uniqueness validation for tweet_id and user_id, a user can only like a tweet once
 end
